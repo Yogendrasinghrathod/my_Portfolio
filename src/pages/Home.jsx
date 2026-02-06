42cfa9c7245caf82db748858326a5e83e257d48c
Yogendra Singh Rathod <rathodyogi91221@gmail.com>
2026-02-07 01:49:57 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,34 @@
-import { lazy, Suspense } from "react";
-
 import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
-
-/* Lazy-loaded sections */
-
-import {StarBackground} from "../components/StarBackground"
-
-const AboutSection = lazy(() =>
-  import("../components/AboutSection")
-);
-const SkillsSection = lazy(() =>
-  import("../components/SkillsSection")
-);
-const ProjectsSection = lazy(() =>
-  import("../components/ProjectsSection")
-);
-const ContactSection = lazy(() =>
-  import("../components/ContactSection")
-);
-const Footer = lazy(() =>
-  import("../components/Footer")
-);
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
       <ThemeToggle />
-      <Navbar />
+      {/* Background Effects */}
+      <StarBackground />
 
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
       <main>
         <HeroSection />
-
-        <Suspense fallback={null}>
-          <StarBackground />
-          <AboutSection />
-          <SkillsSection />
-          <ProjectsSection />
-          <ContactSection />
-        </Suspense>
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
       </main>
 
-      <Suspense fallback={null}>
-        <Footer />
-      </Suspense>
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
